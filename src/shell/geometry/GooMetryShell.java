e85c857f9f6c9ae589bb42e24305f4a7752d7e80
xin81 <xin81@nurfuerspam.de>
2015-02-28 19:02:14 +0100

    update License text

--- src/shell/geometry/GooMetryShell.java
+++ src/shell/geometry/GooMetryShell.java
@@ -1,6 +1,6 @@
 package shell.geometry;
 /*
-Copyright [2015] [Nguyen Viet Tan]
+Copyright 2015 Nguyen Viet Tan
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
